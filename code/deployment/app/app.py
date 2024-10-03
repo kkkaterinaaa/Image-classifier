fadc1151971fe0bfca1b55e84c625a897ea55d66
Ekaterina Zaitseva <57675182+kkkaterinaaa@users.noreply.github.com>
2024-10-03 23:27:21 +0300

    predict button

--- code/deployment/app/app.py
+++ code/deployment/app/app.py
@@ -6,17 +6,19 @@ import io
 st.title("Image Classification App")
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
+
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
-    img_bytes = io.BytesIO()
-    image.save(img_bytes, format="JPEG")
-    img_bytes = img_bytes.getvalue()
 
-    with st.spinner('Making prediction...'):
-        response = requests.post("http://fastapi-api:8000/predict/", files={"file": img_bytes})
+    if st.button("Predict"):
+        img_bytes = io.BytesIO()
+        image.save(img_bytes, format="JPEG")
+        img_bytes = img_bytes.getvalue()
 
-    if response.status_code == 200:
-        st.success(f"Predicted class: {response.json()['predicted_class']}")
-    else:
-        st.error("Error: Could not get prediction.")
+        with st.spinner('Making prediction...'):
+            response = requests.post("http://fastapi-api:8000/predict/", files={"file": img_bytes})
+        if response.status_code == 200:
+            st.success(f"Predicted class: {response.json()['predicted_class']}")
+        else:
+            st.error("Error: Could not get prediction.")
